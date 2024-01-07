85ebbcd6bae76432d3228e33fb6f9035b46f8d2a
zestbear <zestbear90@gmail.com>
2024-01-08 00:44:03 +0900

    :sparkles: feature: Asset Entity와 Member 연결 테이블 설정

--- src/main/java/com/automated/trading/stock/StockManager/asset/domain/Asset.java
+++ src/main/java/com/automated/trading/stock/StockManager/asset/domain/Asset.java
@@ -1,4 +1,22 @@
 package com.automated.trading.stock.StockManager.asset.domain;
 
+import com.automated.trading.stock.StockManager.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "asset_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
 }
