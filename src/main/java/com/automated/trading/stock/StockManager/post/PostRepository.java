6a6b642b516c81ac57c33b4e0d8c984c4db68d6d
zestbear <zestbear90@gmail.com>
2024-01-07 18:27:14 +0900

    :sparkles: feature: 게시물 Repository

---
+++ src/main/java/com/automated/trading/stock/StockManager/post/PostRepository.java
@@ -0,0 +1,6 @@
+package com.automated.trading.stock.StockManager.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Long, Post> {
+}
