8c7992652c939605526747a600b353a8d12bb0a0
zestbear <zestbear90@gmail.com>
2024-01-11 16:46:34 +0900

    :sparkles: feature: @Repository 추가

--- src/main/java/com/automated/trading/stock/StockManager/member/domain/MemberRepository.java
+++ src/main/java/com/automated/trading/stock/StockManager/member/domain/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.automated.trading.stock.StockManager.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Long, Member> {
 }
