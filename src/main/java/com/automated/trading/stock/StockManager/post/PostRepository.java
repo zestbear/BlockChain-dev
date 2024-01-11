897f5850911faf0b8a2b432f664093c6bbd70386
zestbear <zestbear90@gmail.com>
2024-01-11 20:48:50 +0900

    :ambulance: fix: JpaRepository 오류 수정

--- src/main/java/com/automated/trading/stock/StockManager/post/PostRepository.java
+++ src/main/java/com/automated/trading/stock/StockManager/post/PostRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Long, Post> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
