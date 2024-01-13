4a1ce14ceffa4050b8af0dfdb359705112bd87a6
zestbear <zestbear90@gmail.com>
2024-01-13 17:24:56 +0900

    :sparkles: feature: Id와 loginID로 Member 반환하는 쿼리 작성

--- src/main/java/com/automated/trading/stock/StockManager/member/domain/MemberRepository.java
+++ src/main/java/com/automated/trading/stock/StockManager/member/domain/MemberRepository.java
@@ -1,8 +1,19 @@
 package com.automated.trading.stock.StockManager.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT m FROM Member m WHERE m.id = :memberId")
+    Optional<Member> findById(@Param("memberId") Long memberId);
+
+    @Query("SELECT m FROM Member m WHERE m.loginID = :loginID")
+    Optional<Member> findByMemberID(@Param("loginID") String loginID);
+
 }
