a990834d10064684cbbcda391a2d956d9b4d8c7c
zestbear <zestbear90@gmail.com>
2024-01-14 16:42:05 +0900

    :sparkles: feature: Hash 생성 기능 추가

--- src/main/java/com/automated/trading/stock/StockManager/block/service/BlockServiceImpl.java
+++ src/main/java/com/automated/trading/stock/StockManager/block/service/BlockServiceImpl.java
@@ -1,4 +1,33 @@
 package com.automated.trading.stock.StockManager.block.service;
 
+import com.automated.trading.stock.StockManager.block.dto.GenerateHashDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+@Transactional
 public class BlockServiceImpl implements BlockService {
+
+    @Override
+    public String generateHash(GenerateHashDto generateHashDto) {
+        try {
+            String combinedData = generateHashDto.getPreviousHash() + generateHashDto.getData() + generateHashDto.getTimeStamp();
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(combinedData.getBytes());
+
+            StringBuilder hashString = new StringBuilder();
+            for (byte b : hash) {
+                hashString.append(String.format("%02x", b));
+            }
+
+            return hashString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return e.toString();
+        }
+    }
+
 }
