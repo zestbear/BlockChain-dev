da3529d23000348fd5c60f42fb78d65535671cbe
zestbear <zestbear90@gmail.com>
2024-01-14 19:43:46 +0900

    :sparkles: feature: 암호화 기능 구현 (keypair 암호화 및 복호화)

---
+++ src/main/java/com/automated/trading/stock/StockManager/util/config/Encryption.java
@@ -0,0 +1,45 @@
+package com.automated.trading.stock.StockManager.util.config;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+
+@Service
+@NoArgsConstructor
+public class Encryption {
+
+    private static final String ALGORITHM = "AES";
+    private String KEY = ""; // TODO: 환경변수 설정
+
+    public String encrypt(String data) throws Exception {
+        try {
+            Key key = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encVal = c.doFinal(data.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encVal);
+        } catch (Exception e) {
+            throw new Exception("Error while encrypting: " + e.toString());
+        }
+    }
+
+    public String decrypt(String encryptedData) throws Exception {
+        try {
+            Key key = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+            Cipher c = Cipher.getInstance(ALGORITHM);
+            c.init(Cipher.DECRYPT_MODE, key);
+            byte[] decodedValue = Base64.getDecoder().decode(encryptedData.getBytes(StandardCharsets.UTF_8));
+            byte[] decValue = c.doFinal(decodedValue);
+            return new String(decValue, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new Exception("Error while decrypting: " + e.toString());
+        }
+    }
+
+}
