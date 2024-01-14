cade9fb20c074a81de687055beead69d14d13101
zestbear <zestbear90@gmail.com>
2024-01-14 19:31:07 +0900

    :building_construction: fix: WalletService을 확장성을 고려하여 인터페이스로 변경

--- src/main/java/com/automated/trading/stock/StockManager/wallet/service/WalletService.java
+++ src/main/java/com/automated/trading/stock/StockManager/wallet/service/WalletService.java
@@ -1,68 +1,11 @@
 package com.automated.trading.stock.StockManager.wallet.service;
 
-import com.automated.trading.stock.StockManager.wallet.domain.WalletRepository;
-import lombok.Getter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.automated.trading.stock.StockManager.wallet.dto.SearchKeyPairDto;
 
-import java.security.*;
-import java.security.spec.ECGenParameterSpec;
+public interface WalletService {
 
-@Service
-@Getter
-public class WalletService {
+    public String[] createKeypair();
 
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
-
-    private PrivateKey key;
-
-    private String privateKey;
-    private String publicXKey;
-    private String publicYKey;
-
-    private final WalletRepository walletRepository;
-
-    public WalletService(WalletRepository walletRepository) {
-        this.walletRepository = walletRepository;
-    }
-
-    @Transactional
-    public void createKeypair() {
-
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
-            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
-
-            keyPairGenerator.initialize(ecGenParameterSpec, secureRandom);
-
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-            key = keyPair.getPrivate();
-
-            String parsing = key.toString();
-            parsing = parsing.replace("EC Private Key", "");
-            parsing = parsing.replaceAll(" ", "");
-            parsing = parsing.replaceAll("X:", "");
-            parsing = parsing.replaceAll("Y:", "");
-            parsing = parsing.replaceAll(":", "");
-            parsing = parsing.replaceAll("\\[", "");
-            parsing = parsing.replaceAll("]", "");
-
-            String[] parsed = new String[3];
-            parsed = parsing.split("\n");
-
-            privateKey = parsed[0];
-            publicXKey = parsed[1];
-            publicYKey = parsed[2];
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
+    public Boolean checkKey(SearchKeyPairDto searchKeyPairDto);
 
 }
