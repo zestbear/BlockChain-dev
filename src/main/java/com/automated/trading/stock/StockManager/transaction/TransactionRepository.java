818e60e99772f2bc55140608cb9e2e8acf295092
DESKTOP-D6CN8EI\humanf <zestbear@humanf.co.kr>
2024-01-16 17:51:31 +0900

    :sparkles: feature: Repository Bean 등록

--- src/main/java/com/automated/trading/stock/StockManager/transaction/TransactionRepository.java
+++ src/main/java/com/automated/trading/stock/StockManager/transaction/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.automated.trading.stock.StockManager.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
