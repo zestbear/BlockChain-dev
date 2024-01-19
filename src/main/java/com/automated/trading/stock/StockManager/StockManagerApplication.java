e6e3312340d7e1d200903ed104ce0353e16ad744
zestbear <zestbear90@gmail.com>
2024-01-19 22:47:04 +0900

    :sparkles: feature: 환경변수 Scheduled로 일정 주기마다 출력하여 확인

--- src/main/java/com/automated/trading/stock/StockManager/StockManagerApplication.java
+++ src/main/java/com/automated/trading/stock/StockManager/StockManagerApplication.java
@@ -2,7 +2,9 @@ package com.automated.trading.stock.StockManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class StockManagerApplication {
 
