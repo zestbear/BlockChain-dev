0aa3f9049d01102351abb5a5d33647b46ab8f570
zestbear <zestbear90@gmail.com>
2024-01-19 22:41:55 +0900

    :sparkles: feature: properties의 환경변수 @Value로 가져오기

---
+++ src/main/java/com/automated/trading/stock/StockManager/util/config/EnvConfig.java
@@ -0,0 +1,8 @@
+package com.automated.trading.stock.StockManager.util.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+}
