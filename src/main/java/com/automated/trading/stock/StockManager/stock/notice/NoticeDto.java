ac8966ed3cc46b88bddfdc9ac50be2454ae14de2
zestbear <zestbear90@gmail.com>
2024-01-21 22:50:27 +0900

    :sparkles: feature: Dto에 작성자 id(member_id)와 수정시간(mod_dt) 추가

--- src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeDto.java
@@ -3,15 +3,16 @@ package com.automated.trading.stock.StockManager.stock.notice;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 public class NoticeDto {
 
     private int notice_id;
+    private int member_id;
     private String title;
     private String content;
     private String post_type;
     private String wrt_dt;
+    private String mod_dt;
     private Boolean imprtnc;
 
 }
