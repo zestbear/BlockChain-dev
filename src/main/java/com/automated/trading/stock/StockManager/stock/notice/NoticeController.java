969f3e4be7bceb0ff4b569fc66f6cde1a090aa6b
zestbear <zestbear90@gmail.com>
2024-01-21 22:50:55 +0900

    :sparkles: feature: 공지사항 crud Controller 구현

--- src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeController.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeController.java
@@ -1,6 +1,8 @@
 package com.automated.trading.stock.StockManager.stock.notice;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class NoticeController {
@@ -9,6 +11,41 @@ public class NoticeController {
 
     public NoticeController(NoticeService noticeService) {
         this.noticeService = noticeService;
+    }
+
+    @PostMapping("/notice/save/{member_id}")
+    public void saveNotice(@PathVariable("member_id") int member_id,
+                           @RequestBody NoticeDto dto) {
+        dto.setMember_id(member_id);
+        noticeService.saveNotice(dto);
+    }
+
+    @PutMapping("/notice/update/{member_id}/{notice_id}")
+    public void updateNotice(@PathVariable("member_id") int member_id,
+                             @PathVariable("notice_id") int notice_id,
+                             @RequestBody NoticeDto dto) {
+        dto.setMember_id(member_id);
+        dto.setNotice_id(notice_id);
+        noticeService.updateNotice(dto);
+    }
+
+    @DeleteMapping("/notice/delete/{member_id}/{notice_id}")
+    public void deleteNotice(@PathVariable("member_id") int member_id,
+                             @PathVariable("notice_id") int notice_id) {
+        NoticeDto dto = new NoticeDto();
+        dto.setMember_id(member_id);
+        dto.setNotice_id(notice_id);
+        noticeService.deleteNotice(dto);
+    }
+
+    @PostMapping("/notice/getAll")
+    public List<NoticeDto> findAllNotices() {
+        return noticeService.findAllNotices();
+    }
+
+    @PostMapping("/notice/getNotice/{notice_id}")
+    public NoticeDto findById(@PathVariable("notice_id") int notice_id) {
+        return noticeService.findById(notice_id);
     }
 
 }
