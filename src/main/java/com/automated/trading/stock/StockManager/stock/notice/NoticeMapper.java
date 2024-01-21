45553d9add74f85f67d1362261994ca6f3ba282a
zestbear <zestbear90@gmail.com>
2024-01-21 18:50:03 +0900

    :sparkles: feature: 공지사항 Mapper 인터페이스 추가

--- src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeMapper.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/notice/NoticeMapper.java
@@ -2,6 +2,19 @@ package com.automated.trading.stock.StockManager.stock.notice;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface NoticeMapper {
+
+    List<NoticeDto> findAllNotices();
+
+    void saveNotice(NoticeDto dto);
+
+    void updateNotice(NoticeDto dto);
+
+    void deleteNotice(int notice_id);
+
+    NoticeDto findById(int notice_id);
+
 }
