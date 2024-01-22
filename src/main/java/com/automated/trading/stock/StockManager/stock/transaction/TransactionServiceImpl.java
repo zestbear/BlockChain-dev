43ca4a183f2abbc3e7a0c6024f5efdd6962b4d54
DESKTOP-D6CN8EI\humanf <zestbear@humanf.co.kr>
2024-01-22 13:04:53 +0900

    :sparkles: feature: 거래 Service Impl 작성 (추후 기능 추가 가능성 있음)

--- src/main/java/com/automated/trading/stock/StockManager/stock/transaction/TransactionServiceImpl.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/transaction/TransactionServiceImpl.java
@@ -1,4 +1,48 @@
 package com.automated.trading.stock.StockManager.stock.transaction;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionMapper transactionMapper;
+
+    public TransactionServiceImpl(TransactionMapper transactionMapper) {
+        this.transactionMapper = transactionMapper;
+    }
+
+    @Override
+    public void transactionOccurred(TransactionDto dto) {
+        transactionMapper.transactionOccurred(dto);
+    }
+
+    @Override
+    public List<TransactionDto> getAllTransactions() {
+        return transactionMapper.getAllTransactions();
+    }
+
+    @Override
+    public TransactionDto findBySenderKey(String sender_key) {
+        return transactionMapper.findBySenderKey(sender_key);
+    }
+
+    @Override
+    public TransactionDto findByReceiverKey(String receiver_key) {
+        return transactionMapper.findByReceiverKey(receiver_key);
+    }
+
+    @Override
+    public TransactionDto findBySignature(String signature) {
+        return transactionMapper.findBySignature(signature);
+    }
+
+    @Override
+    public List<TransactionDto> findByDateTime(String start_dt, String end_dt) {
+        return transactionMapper.findByDateTime(start_dt, end_dt);
+    }
+
 }
