fad95538c043053ea1bee2641daf52e7bafbeef1
DESKTOP-D6CN8EI\humanf <zestbear@humanf.co.kr>
2024-01-22 14:17:45 +0900

    :white_check_mark: test: 기본 endPoint에 대한 연결 html 맵핑

--- src/main/java/com/automated/trading/stock/StockManager/indexController.java
+++ src/main/java/com/automated/trading/stock/StockManager/indexController.java
@@ -1,15 +1,17 @@
 package com.automated.trading.stock.StockManager;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class indexController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @GetMapping("/")
+    public String defaultPage() {
+        return "default";
+    }
+
+    @GetMapping("/index")
     public String index() {
         return "index";
     }
