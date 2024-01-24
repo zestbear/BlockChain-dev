bf234b2d9b9c00387f9433cb91053e703146dd9e
zestbear <zestbear90@gmail.com>
2024-01-24 10:49:04 +0900

    :recycle: fix: 공지사항 Dto 수정

--- src/main/java/com/automated/trading/stock/StockManager/stock/post/notice/NoticeDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/post/notice/NoticeDto.java
@@ -1,17 +1,16 @@
 package com.automated.trading.stock.StockManager.stock.post.notice;
 
-import com.automated.trading.stock.StockManager.stock.post.PostType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class NoticeDto {
 
-    private int notice_id;
+    private int post_fk;
     private int member_id;
     private String title;
     private String content;
-    private PostType post_type;
+    private String post_type;
     private String wrt_dt;
     private String mod_dt;
     private Boolean imprtnc;
