a8061eb7e87c20f8170e253dfafa1286ea8b9175
zestbear <zestbear90@gmail.com>
2024-01-25 16:11:03 +0900

    :sparkles: feature: 공지사항 Dto 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/post/notice/dto/NoticeDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/post/notice/dto/NoticeDto.java
@@ -1,18 +1,19 @@
-package com.automated.trading.stock.StockManager.stock.post.notice;
+package com.automated.trading.stock.StockManager.stock.post.notice.dto;
 
+import com.automated.trading.stock.StockManager.stock.post.PostType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class NoticeDto {
 
-    private int post_fk;
     private int member_id;
     private String title;
     private String content;
-    private String post_type;
     private String wrt_dt;
     private String mod_dt;
     private Boolean imprtnc;
+    private PostType post_type;
+    private int post_fk;
 
 }
