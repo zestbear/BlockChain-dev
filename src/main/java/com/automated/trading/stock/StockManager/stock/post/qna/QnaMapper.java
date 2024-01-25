6e4b4b5f6886b5d21427917b5804a1f47b3b1242
zestbear <zestbear90@gmail.com>
2024-01-25 16:10:12 +0900

    :truck: fix: Q&A Dto 별도 폴더로 이동

--- src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/QnaMapper.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/QnaMapper.java
@@ -1,5 +1,6 @@
 package com.automated.trading.stock.StockManager.stock.post.qna;
 
+import com.automated.trading.stock.StockManager.stock.post.qna.dto.QnaDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
--- src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/dto/QnaDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/dto/QnaDto.java
@@ -1,4 +1,4 @@
-package com.automated.trading.stock.StockManager.stock.post.qna;
+package com.automated.trading.stock.StockManager.stock.post.qna.dto;
 
 import lombok.Getter;
 import lombok.Setter;
