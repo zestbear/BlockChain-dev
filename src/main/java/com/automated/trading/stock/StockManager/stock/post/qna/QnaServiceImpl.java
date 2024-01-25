bcd1923b9e255555f9829291d78ace041043f394
zestbear <zestbear90@gmail.com>
2024-01-25 17:39:29 +0900

    :sparkles: feature: Q&A 기능 구현 (controller, mapper, service)

--- src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/QnaServiceImpl.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/post/qna/QnaServiceImpl.java
@@ -1,21 +1,45 @@
 package com.automated.trading.stock.StockManager.stock.post.qna;
 
 import com.automated.trading.stock.StockManager.stock.post.notice.dto.NoticeSaveRequestDto;
-import com.automated.trading.stock.StockManager.stock.post.qna.dto.QnaDto;
+import com.automated.trading.stock.StockManager.stock.post.qna.dto.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class QnaServiceImpl implements QnaService {
 
-    @Override
-    public void postSave(QnaDto dto) {
+    private final QnaMapper qnaMapper;
+
+    public QnaServiceImpl(QnaMapper qnaMapper) {
+        this.qnaMapper = qnaMapper;
     }
 
     @Override
-    public void postUpdate(NoticeSaveRequestDto dto) {
+    public void saveQna(QnaSaveRequestDto dto) {
+        qnaMapper.saveQna(dto);
     }
 
     @Override
-    public void postDelete(Long postId) {
+    public void updateQna(QnaUpdateRequestDto dto) {
+        qnaMapper.updateQna(dto);
     }
+
+    @Override
+    public void deleteQna(QnaDeleteRequestDto dto) {
+        qnaMapper.deleteQna(dto);
+    }
+
+    @Override
+    public List<QnaListResponseDto> findAllQnas() {
+        return qnaMapper.findAllQnas();
+    }
+
+    @Override
+    public QnaListResponseDto findById(int qna_id) {
+        return qnaMapper.findById(qna_id);
+    }
+
 }
