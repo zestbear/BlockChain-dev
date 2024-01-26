4435e5b4b9e6958dec8d6943ee3d0494a2ab814b
zestbear <zestbear90@gmail.com>
2024-01-26 16:52:56 +0900

    :adhesive_bandage: fix: Dto 기능 분리에 따른 매개변수 수정

--- src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockMapper.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockMapper.java
@@ -1,15 +1,25 @@
 package com.automated.trading.stock.StockManager.stock.block;
 
-import com.automated.trading.stock.StockManager.stock.block.dto.BlockDto;
+import com.automated.trading.stock.StockManager.stock.block.dto.BlockSaveRequestDto;
+import com.automated.trading.stock.StockManager.stock.block.dto.BlockUpdateRequestDto;
+import com.automated.trading.stock.StockManager.stock.block.dto.ReturnBlockResponseDto;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface BlockMapper {
 
-    void createBlock(BlockDto dto);
+    void createBlock(BlockSaveRequestDto dto);
 
-    void updateBlock(int block_id);
+    void updateBlock(BlockUpdateRequestDto dto);
 
-    void deleteBlock(BlockDto dto);
+    void deleteBlock(int block_id);
 
-    BlockDto findById(int block_id);
+    ReturnBlockResponseDto findByHash(String hash);
+
+    ReturnBlockResponseDto findByPreviousHash(String previous_hash);
+
+    int countBlocks();
+
+    String getLastHash();
 
 }
