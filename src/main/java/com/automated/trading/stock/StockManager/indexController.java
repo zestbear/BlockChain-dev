6a24f0489179489836c80bbd1f45a10ab5829dc7
zestbear <zestbear90@gmail.com>
2024-01-26 14:07:42 +0900

    :fire: remove: defaultController 삭제 및 default.html 삭제

--- src/main/java/com/automated/trading/stock/StockManager/indexController.java
+++ src/main/java/com/automated/trading/stock/StockManager/indexController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-    @GetMapping("/")
-    public String defaultPage() {
-        return "default";
-    }
-
     @GetMapping("/index")
     public String index() {
         return "index";
