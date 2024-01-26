ae2178960194e2908def4d44dba6dc28f90fdd97
zestbear <zestbear90@gmail.com>
2024-01-26 12:31:48 +0900

    :sparkles: feature: Security 인증 없이 접근 가능한 Controller 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactController.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactController.java
@@ -1,6 +1,7 @@
 package com.automated.trading.stock.StockManager.stock.contact;
 
 import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,14 @@ public class ContactController {
 
     @PostMapping("/contact/send")
     public RequestEntity<String> sendContact() {
+        return null;
+    }
+
+    /*
+        Contact는 Spring Security에서 Security 없이 접근할 수 있다.
+     */
+    @GetMapping("/contact")
+    public RequestEntity<String> findAllContacts() {
         return null;
     }
 
