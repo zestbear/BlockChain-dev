8187fbc35b3f90863e22a9c2844e7db04ea92aac
zestbear <zestbear90@gmail.com>
2024-01-26 16:55:06 +0900

    :sparkles: feature: Block 관련 필요한 기능들 추가

--- src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockService.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockService.java
@@ -1,11 +1,24 @@
 package com.automated.trading.stock.StockManager.stock.block;
 
+import com.automated.trading.stock.StockManager.stock.block.dto.BlockSaveRequestDto;
+import com.automated.trading.stock.StockManager.stock.block.dto.BlockUpdateRequestDto;
 import com.automated.trading.stock.StockManager.stock.block.dto.GenerateHashDto;
+import com.automated.trading.stock.StockManager.stock.block.dto.ReturnBlockResponseDto;
 
 public interface BlockService {
 
     public String generateHash(GenerateHashDto generateHashDto);
 
 //    public void generateBlock(String hash) throws JsonProcessingException;
+
+    public void createBlock(BlockSaveRequestDto dto);
+
+    public void updateBlock(BlockUpdateRequestDto dto);
+
+    public void deleteBlock(int block_id);
+
+    public ReturnBlockResponseDto findByHash(String hash);
+
+    public ReturnBlockResponseDto findByPreviousHash(String previous_hash);
 
 }
