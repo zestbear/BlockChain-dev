dd30bf218f79ab68c99b3ce71dce2b2d55cac509
zestbear <zestbear90@gmail.com>
2024-01-26 10:59:01 +0900

    :sparkles: feature: Contact Service와 ServiceImpl 작성

---
+++ src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactServiceImpl.java
@@ -0,0 +1,7 @@
+package com.automated.trading.stock.StockManager.stock.contact;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+}
