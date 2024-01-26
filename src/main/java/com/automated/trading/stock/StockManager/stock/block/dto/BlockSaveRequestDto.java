4c626b3c0aa6864e6ededecec41d6592fd749007
zestbear <zestbear90@gmail.com>
2024-01-26 16:53:45 +0900

    :sparkles: feature: Block 생성 위한 Dto 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/block/dto/BlockSaveRequestDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/block/dto/BlockSaveRequestDto.java
@@ -1,4 +1,17 @@
 package com.automated.trading.stock.StockManager.stock.block.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class BlockSaveRequestDto {
+
+    private String hash;
+    private String previous_hash;
+    private String time_stamp;
+    private int member_id;
+
 }
