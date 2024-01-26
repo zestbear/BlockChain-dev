d6b93dd6cf36254fc2d249544744d2e946a7a0e2
zestbear <zestbear90@gmail.com>
2024-01-26 15:17:46 +0900

    :adhesive_bandage: fix: class -> interface 수정

--- src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockMapper.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/block/BlockMapper.java
@@ -1,4 +1,15 @@
 package com.automated.trading.stock.StockManager.stock.block;
 
-public class BlockMapper {
+import com.automated.trading.stock.StockManager.stock.block.dto.BlockDto;
+
+public interface BlockMapper {
+
+    void createBlock(BlockDto dto);
+
+    void updateBlock(int block_id);
+
+    void deleteBlock(BlockDto dto);
+
+    BlockDto findById(int block_id);
+
 }
