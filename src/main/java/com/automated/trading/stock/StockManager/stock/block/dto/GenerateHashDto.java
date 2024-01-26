5abdb69de25ab39197b407bbf47e3e083392e28b
zestbear <zestbear90@gmail.com>
2024-01-26 15:08:16 +0900

    :truck: rename: hash 생성 dto 폴더 이동

--- src/main/java/com/automated/trading/stock/StockManager/stock/block/dto/GenerateHashDto.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/block/dto/GenerateHashDto.java
@@ -1,8 +1,9 @@
-package com.automated.trading.stock.StockManager.stock.block;
+package com.automated.trading.stock.StockManager.stock.block.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
-@Getter
+@Getter @Setter
 public class GenerateHashDto {
 
     private String previousHash;
