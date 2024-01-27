7ea06b7f8acb6336bcc54ffad535e43b48fa8cc3
zestbear <zestbear90@gmail.com>
2024-01-27 23:57:39 +0900

    :sparkles: feature: Contact Mapper 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactMapper.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactMapper.java
@@ -1,4 +1,21 @@
 package com.automated.trading.stock.StockManager.stock.contact;
 
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSendRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSetStateRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ReturnContactResponseDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface ContactMapper {
+
+    void sendContact(ContactSendRequestDto dto);
+
+    void respondContact(ContactSetStateRequestDto dto);
+
+    List<ReturnContactResponseDto> findAllContacts();
+
+    ReturnContactResponseDto findById(int contact_id);
+
 }
