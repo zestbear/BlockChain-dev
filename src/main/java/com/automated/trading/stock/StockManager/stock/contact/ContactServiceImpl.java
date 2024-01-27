a7a1ad8199192634926ea37ca63c206563ce89e8
zestbear <zestbear90@gmail.com>
2024-01-27 23:57:30 +0900

    :sparkles: feature: Contact 기능 Serivce 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactServiceImpl.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactServiceImpl.java
@@ -1,7 +1,41 @@
 package com.automated.trading.stock.StockManager.stock.contact;
 
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSendRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSetStateRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ReturnContactResponseDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class ContactServiceImpl implements ContactService {
+
+    private final ContactMapper contactMapper;
+
+    public ContactServiceImpl(ContactMapper contactMapper) {
+        this.contactMapper = contactMapper;
+    }
+
+    @Override
+    public void sendContact(ContactSendRequestDto dto) {
+        contactMapper.sendContact(dto);
+    }
+
+    @Override
+    public void respondContact(ContactSetStateRequestDto dto) {
+        contactMapper.respondContact(dto);
+    }
+
+    @Override
+    public List<ReturnContactResponseDto> findAllContacts() {
+        return contactMapper.findAllContacts();
+    }
+
+    @Override
+    public ReturnContactResponseDto findById(int contact_id) {
+        return contactMapper.findById(contact_id);
+    }
+
 }
