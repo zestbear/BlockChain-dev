a3269ef9623ff1e6bf0cd51e2cd6e0713f16297d
zestbear <zestbear90@gmail.com>
2024-01-27 23:56:57 +0900

    :sparkles: feature: Contact Controller 작성

--- src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactController.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/contact/ContactController.java
@@ -1,9 +1,12 @@
 package com.automated.trading.stock.StockManager.stock.contact;
 
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSendRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ContactSetStateRequestDto;
+import com.automated.trading.stock.StockManager.stock.contact.dto.ReturnContactResponseDto;
 import org.springframework.http.RequestEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -15,16 +18,26 @@ public class ContactController {
     }
 
     @PostMapping("/contact/send")
-    public RequestEntity<String> sendContact() {
+    public RequestEntity<String> sendContact(@RequestBody ContactSendRequestDto dto) {
         return null;
+    }
+
+    @PutMapping("/contact/respond")
+    public RequestEntity<String> respondContact(@RequestBody ContactSetStateRequestDto dto) {
+        return null;
+    }
+
+    @GetMapping("/contact/{contact_id")
+    public ReturnContactResponseDto findById(@PathVariable("contact_id") int contact_id) {
+        return contactService.findById(contact_id);
     }
 
     /*
         Contact는 Spring Security에서 Security 없이 접근할 수 있다.
      */
     @GetMapping("/contact")
-    public RequestEntity<String> findAllContacts() {
-        return null;
+    public List<ReturnContactResponseDto> findAllContacts() {
+        return contactService.findAllContacts();
     }
 
 }
