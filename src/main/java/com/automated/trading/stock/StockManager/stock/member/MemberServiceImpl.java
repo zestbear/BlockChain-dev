6f5f5987da9831921999b73448b3e2f2dc4c060c
zestbear <zestbear90@gmail.com>
2024-02-06 17:12:55 +0900

    :sparkles: feature: 사용자 회원가입 기능 구현 [완료]

--- src/main/java/com/automated/trading/stock/StockManager/stock/member/MemberServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.automated.trading.stock.StockManager.stock.member;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class MemberServiceImpl implements MemberService {
-}
---
+++ src/main/java/com/automated/trading/stock/StockManager/util/exception/MemberDuplicatedException.java
@@ -0,0 +1,9 @@
+package com.automated.trading.stock.StockManager.util.exception;
+
+public class MemberDuplicatedException extends RuntimeException {
+
+    public MemberDuplicatedException() {
+        super();
+    }
+
+}
