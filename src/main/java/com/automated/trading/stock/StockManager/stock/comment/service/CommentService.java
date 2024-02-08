567432ab3e25c463b4ccf648517adb788f8b9ceb
zestbear <zestbear90@gmail.com>
2024-02-08 13:30:29 +0900

    :sparkles: feature: 댓글 기능 (채팅작성, 좋아요, 조회) 구현 완료

--- src/main/java/com/automated/trading/stock/StockManager/stock/comment/service/CommentService.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/comment/service/CommentService.java
@@ -2,16 +2,15 @@ package com.automated.trading.stock.StockManager.stock.comment.service;
 
 import com.automated.trading.stock.StockManager.stock.comment.controller.dto.CommentSaveRequestDto;
 import com.automated.trading.stock.StockManager.stock.comment.controller.dto.ReturnCommentResponseDto;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface CommentService {
 
-    void saveComment(Long qna_id, CommentSaveRequestDto dto);
+    void saveComment(Long qna_id, Long member_id, CommentSaveRequestDto dto);
 
     List<ReturnCommentResponseDto> findAllComments(Long qna_id);
 
-    void addRecommends(Long comment_id);
+    int addRecommends(Long comment_id);
 
 }
--- src/main/java/com/automated/trading/stock/StockManager/stock/comment/service/CommentServiceImpl.java
+++ src/main/java/com/automated/trading/stock/StockManager/stock/comment/service/CommentServiceImpl.java
@@ -4,6 +4,13 @@ import com.automated.trading.stock.StockManager.stock.comment.controller.dto.Com
 import com.automated.trading.stock.StockManager.stock.comment.controller.dto.ReturnCommentResponseDto;
 import com.automated.trading.stock.StockManager.stock.comment.domain.Comment;
 import com.automated.trading.stock.StockManager.stock.comment.domain.CommentRepository;
+import com.automated.trading.stock.StockManager.stock.member.domain.Member;
+import com.automated.trading.stock.StockManager.stock.member.domain.MemberRepository;
+import com.automated.trading.stock.StockManager.stock.qna.domain.Qna;
+import com.automated.trading.stock.StockManager.stock.qna.domain.QnaRepository;
+import com.automated.trading.stock.StockManager.util.exception.CommentNotExistException;
+import com.automated.trading.stock.StockManager.util.exception.MemberNotExistException;
+import com.automated.trading.stock.StockManager.util.exception.PostNotExistException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,16 +21,42 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final QnaRepository qnaRepository;
 
-    public CommentServiceImpl(CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, MemberRepository memberRepository, QnaRepository qnaRepository) {
         this.commentRepository = commentRepository;
+        this.memberRepository = memberRepository;
+        this.qnaRepository = qnaRepository;
     }
 
+    /**
+     * 댓글 저장
+     *
+     * @param qna_id
+     * @param member_id
+     * @param dto
+     */
     @Override
     @Transactional
-    public void saveComment(Long qna_id, CommentSaveRequestDto dto) {
+    public void saveComment(Long qna_id, Long member_id, CommentSaveRequestDto dto) {
+        Member member = memberRepository.findById(member_id)
+                .orElseThrow(MemberNotExistException::new);
+        Qna qna = qnaRepository.findById(qna_id)
+                .orElseThrow(PostNotExistException::new);
+
+        commentRepository.save(Comment.builder()
+                .content(dto.getContent())
+                .member(member)
+                .qna(qna)
+                .build());
     }
 
+    /**
+     * 댓글 모두 반환
+     * @param qna_id
+     * @return
+     */
     @Override
     public List<ReturnCommentResponseDto> findAllComments(Long qna_id) {
         List<Comment> allComment = commentRepository.findAllComment(qna_id);
@@ -35,8 +68,17 @@ public class CommentServiceImpl implements CommentService {
         return returnComment;
     }
 
+    /**
+     * 댓글에 추천 누르기 (좋아요 기능)
+     * @param comment_id
+     */
     @Override
     @Transactional
-    public void addRecommends(Long comment_id) {
+    public int addRecommends(Long comment_id) {
+        Comment comment = commentRepository.findById(comment_id)
+                .orElseThrow(CommentNotExistException::new);
+        int added = comment.getRecommends() + 1;
+
+        return commentRepository.addRecommend(comment_id, added);
     }
 }
