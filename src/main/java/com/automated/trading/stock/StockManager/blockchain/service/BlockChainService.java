eefb19b25671c8902143202e3cc542405e7ecb57
zestbear <zestbear90@gmail.com>
2024-02-23 11:27:21 +0900

    :sparkles: feature: Transaction 생성 및 저장 기능 구현

--- src/main/java/com/automated/trading/stock/StockManager/blockchain/service/BlockChainService.java
+++ src/main/java/com/automated/trading/stock/StockManager/blockchain/service/BlockChainService.java
@@ -1,15 +1,21 @@
 package com.automated.trading.stock.StockManager.blockchain.service;
 
 import com.automated.trading.stock.StockManager.blockchain.controller.dto.GenerateHashDto;
+import com.automated.trading.stock.StockManager.blockchain.controller.dto.TransactionRequestDto;
+import com.automated.trading.stock.StockManager.blockchain.domain.Wallet;
 
 public interface BlockChainService {
 
+    /** Block 관련 기능 */
     String generateHash(GenerateHashDto generateHashDto);
-
     void createBlock(int member_id);
-
     void updateHash();
-
     void deleteBlock(int block_id);
+
+    /** Wallet 및 Transaction 관련 기능 */
+    String[] createKeypair();
+    Wallet createWallet(int member_id, String[] keys);
+    void checkAuthority(int member_id, String input) throws Exception;
+    void executeTransaction(String senderKey, String receiverKey, TransactionRequestDto dto);
 
 }
