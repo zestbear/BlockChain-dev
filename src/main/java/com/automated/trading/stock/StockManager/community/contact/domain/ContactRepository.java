1efd45385056eb2f77c40963860a7fb5c9f67d75
zestbear <zestbear90@gmail.com>
2024-03-05 12:26:06 +0900

    :sparkles: feature: Contact의 jpa repository 인터페이스 생성 #53

---
+++ src/main/java/com/automated/trading/stock/StockManager/community/contact/domain/ContactRepository.java
@@ -0,0 +1,6 @@
+package com.automated.trading.stock.StockManager.community.contact.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
